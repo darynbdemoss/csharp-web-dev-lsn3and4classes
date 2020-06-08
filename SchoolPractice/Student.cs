0e2c621a4f60e69d2900058241fdd3d81bdadd63
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-08 12:06:32 -0400

    Changes to student.cs

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -17,5 +17,34 @@ namespace SchoolPractice
             Gpa = gpa;
             StudentId = studentId;
         }
+
+        public Student(string name, int studentId)
+        : this(name, studentId, 0, 0) { }
+
+        public Student(string name)
+        : this(name, nextStudentId)
+        {
+            nextStudentId++;
+        }
+
+        // TODO: Complete the AddGrade method.
+        public void AddGrade(int courseCredits, double grade)
+        {
+            // Update the appropriate properties: NumberOfCredits, Gpa
+        }
+
+        //TODO: Complete the GetGradeLevel method here:
+        public string GetGradeLevel(int credits)
+        {
+            // Determine the grade level of the student based on NumberOfCredits
+            return "grade level tbd";
+        }
+
+        // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
+        //  than just the class fields.
+
+        // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
+        //  Student objects equal.
+
     }
 }
